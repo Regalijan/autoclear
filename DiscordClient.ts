77871a06166c57e9b3d2ffab8ba08cb58f053062
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-04-15 21:29:45 -0400

    Validate embeds before sending

--- DiscordClient.ts
+++ DiscordClient.ts
@@ -2,7 +2,7 @@ import { AkairoClient, CommandHandler, ListenerHandler } from 'discord-akairo'
 import { join } from 'path'
 import dotenv from 'dotenv'
 import db from './database'
-import { TextChannel } from 'discord.js'
+import { MessageEmbed, TextChannel } from 'discord.js'
 
 dotenv.config()
 
@@ -53,7 +53,7 @@ setTimeout(async function (): Promise<void> {
 }, 10000)
 
 setInterval(async function (): Promise<void>  {
-  const staleChannels = await db.query('SELECT * FROM channels WHERE CAST(last_ran AS bigint) > CAST($1 AS bigint) + 1800000;', [Date.now()]).catch(e => console.error(e))
+  const staleChannels = await db.query('SELECT * FROM channels WHERE (last_ran + 1800000) < $1;', [Date.now()]).catch(e => console.error(e))
   if (typeof staleChannels === 'undefined') return
   for (let i = 0; i < staleChannels.rowCount; i++) {
     if (staleChannels.rows[i].last_ran + staleChannels.rows[i].interval < Date.now()) continue
@@ -64,10 +64,27 @@ setInterval(async function (): Promise<void>  {
     const channel: TextChannel = untypedChannel
     if (channel.permissionOverwrites.find(p => p.deny.has('MANAGE_MESSAGES'))) continue
     if (!guild.me?.hasPermission('MANAGE_MESSAGES') && !channel.permissionOverwrites.find(p => p.allow.has('MANAGE_MESSAGES'))) continue
-    if ((guild.me?.hasPermission('READ_MESSAGE_HISTORY') && !channel.permissionOverwrites.find(p => p.deny.has('READ_MESSAGE_HISTORY'))) || channel.permissionOverwrites.find(p => p.allow.has('READ_MESSAGE_HISTORY'))) {
-      await channel.messages.fetch({ limit: 500 })
-    }
-    await channel.bulkDelete(500, true)
+    if ((!guild.me?.hasPermission('READ_MESSAGE_HISTORY') || channel.permissionOverwrites.find(p => p.deny.has('READ_MESSAGE_HISTORY'))) && !channel.permissionOverwrites.find(p => p.allow.has('READ_MESSAGE_HISTORY'))) continue
+    while (channel.messages.cache.size > 0 && typeof channel.lastMessage?.createdTimestamp !== 'undefined' && channel.lastMessage.createdTimestamp > Date.now() - 1209600000) await channel.bulkDelete(100, true)
     await db.query('UPDATE channels SET last_ran = $1 WHERE channel = $2;', [Date.now(), channel.id]).catch(e => console.error(e))
+    if (typeof staleChannels.rows[i].pinned_message !== 'undefined') {
+      const embedData = JSON.parse(staleChannels.rows[i].pinned_message)
+      const embed = new MessageEmbed()
+        .setDescription(embedData.description?.toString() ?? '\u200B')
+      if (embedData.author?.name) embed.setAuthor(embedData.author.name.toString(), embedData.author.icon_url.toString(), embedData.author.url.toString())
+      if (embedData.title) embed.setTitle(embedData.title.toString())
+      if (Array.isArray(embedData.fields)) try {
+          embedData.fields.forEach(function (field: { name: string, value: string, inline: boolean | undefined }) {
+          if (field.name && field.value) embed.addField(field.name, field.value, field.inline)
+        })
+      } catch {}
+      if (embedData.image?.url) embed.setImage(embedData.image.url.toString())
+      if (embedData.footer?.text) embed.setFooter(embedData.footer.text.toString(), embedData.footer.icon_url?.toString())
+      if (embedData.thumbnail?.url) embed.setThumbnail(embedData.thumbnail.url.toString())
+      if (embedData.color) try {
+        embed.setColor(parseInt(embedData.color))
+      } catch {}
+      await channel.send(embed)
+    }
   }
 }, 60000)
