c7ee480da24432a771951daefc43af17c396a048
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-04-30 01:09:47 -0400

    Don't send individual requests for each message

--- DiscordClient.ts
+++ DiscordClient.ts
@@ -67,9 +67,11 @@ setInterval(async function (): Promise<void>  {
     if ((!guild.me?.hasPermission('READ_MESSAGE_HISTORY') || channel.permissionOverwrites.find(p => p.deny.has('READ_MESSAGE_HISTORY'))) && !channel.permissionOverwrites.find(p => p.allow.has('READ_MESSAGE_HISTORY'))) continue
     while (channel.messages.cache.filter(msg => !msg.pinned).size > 0 && typeof channel.lastMessage?.createdTimestamp !== 'undefined' && channel.lastMessage.createdTimestamp > Date.now() - 1209600000) {
       const fetchedMsgs = await channel.messages.fetch({ limit: 100 })
+      const ids: string[] = []
       fetchedMsgs.forEach(async msg => {
-        if (!msg.pinned && !msg.deleted) await msg.delete().catch(() => {})
+        if (!msg.pinned && !msg.deleted) ids.push(msg.id)
       })
+      await channel.bulkDelete(ids)
     }
     await db.query('UPDATE channels SET last_ran = $1 WHERE channel = $2;', [Date.now(), channel.id]).catch(e => console.error(e))
   }
