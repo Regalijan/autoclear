83fbc57b57b2c25b2449f28e8a39921ad820baa4
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-05-08 02:48:35 -0400

    Create channelDelete listener

---
+++ listeners/channelDelete.ts
@@ -0,0 +1,19 @@
+import { Listener } from 'discord-akairo'
+import { DMChannel, GuildChannel } from 'discord.js'
+import db from '../database'
+
+export default class ChannelDeleteListener extends Listener {
+  public constructor () {
+    super('channelDelete', {
+      emitter: 'channelDelete',
+      event: 'channelDelete'
+    })
+  }
+
+  public async exec (channel: DMChannel | GuildChannel): Promise<void> {
+    if (channel.type === 'dm') return
+    const foundChannel = await db.query('SELECT * FROM channels WHERE channel = $1;', [channel.id]).catch(e => console.error(e))
+    if (!foundChannel || foundChannel.rowCount === 0) return
+    await db.query('DELETE FROM channels WHERE channel = $1;', [channel.id]).catch(e => console.error(e))
+  }
+}
