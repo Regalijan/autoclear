a6ef4ec33244c1a4185611f10f2b9eb2920457da
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-05-09 20:34:43 -0400

    Remove unused class

--- commands/help.ts
+++ commands/help.ts
@@ -1,5 +1,5 @@
 import { Command } from 'discord-akairo'
-import { Message, MessageEmbed } from 'discord.js'
+import { Message } from 'discord.js'
 
 export default class HelpCommand extends Command {
   public constructor () {
