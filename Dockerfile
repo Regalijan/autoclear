de506eebf8b2699ffc0ece593a42770e2941aa17
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2021-10-04 17:02:25 -0400

    Place table creation logic in index file

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,4 @@ RUN npx tsc
 ENV NODE_ENV=production
 RUN npm prune
 RUN node dist/InteractionsDeploy.js
-RUN psql -U postgres -h postgres autoclear < template.db
 CMD ["node", "dist"]
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { ShardingManager } from 'discord.js'
 import { join } from 'path'
 import dotenv from 'dotenv'
+import { Client as DBClient } from 'pg'
 
 dotenv.config()
 if (typeof process.env.BTKN === 'undefined') {
@@ -16,4 +17,18 @@ shardingManager.on('shardCreate', function (shard) {
   console.log(`Launching shard ${shard.id + 1} of ${shardingManager.totalShards}`)
 })
 
-shardingManager.spawn()
+;(async function () {
+  try {
+    const db = new DBClient({
+      host: process.env.DBH ?? 'postgres',
+      password: process.env.DBPASS,
+      user: process.env.DBU ?? 'postgres',
+      port: 5432
+    }) // Temporary client for main process to create the database if it does not exist
+    await db.connect()
+    await db.query('CREATE DATABASE $1;', [process.env.DBN ?? 'autoclear']).catch(() => {})
+    await db.query('CREATE TABLE IF NOT EXISTS channels (channel text NOT NULL, guild NOT NULL, interval bigint, last_ran bigint, is_insta boolean NOT NULL);')
+    await db.end().catch(() => {})
+  } catch {}
+  await shardingManager.spawn()
+})
